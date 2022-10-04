e50d96bea3ad4870267d3fe7c1a8d1dcce8ece63
Undefined X <undefinedx@UndefinacStudio.attlocal.net>
2022-10-04 12:36:15 -0700

    inital

--- pages/log.js
+++ pages/log.js
@@ -37,7 +37,50 @@ export default function Log(props){
     return(
         <div>
         <Nav objArry={props.elements}/>
+            <div className={"pop"}>
+            <sl-dropdown>
+                <sl-button slot="trigger" caret>Dropdown</sl-button>
+                <sl-menu>
+                    <sl-menu-item>Dropdown Item 1</sl-menu-item>
+                    <sl-menu-item>Dropdown Item 2</sl-menu-item>
+                    <sl-menu-item>Dropdown Item 3</sl-menu-item>
+                    <sl-divider></sl-divider>
+                    <sl-menu-item checked>Checked</sl-menu-item>
+                    <sl-menu-item disabled>Disabled</sl-menu-item>
+                    <sl-divider></sl-divider>
+                    <sl-menu-item>
+                        Prefix
+                        <sl-icon slot="prefix" name="gift"></sl-icon>
+                    </sl-menu-item>
+                    <sl-menu-item>
+                        Suffix Icon
+                        <sl-icon slot="suffix" name="heart"></sl-icon>
+                    </sl-menu-item>
+                </sl-menu>
+            </sl-dropdown>
+            <sl-dropdown>
+                <sl-button slot="trigger" caret>Dropdown</sl-button>
+                <sl-menu>
+                    <sl-menu-item>Dropdown Item 1</sl-menu-item>
+                    <sl-menu-item>Dropdown Item 2</sl-menu-item>
+                    <sl-menu-item>Dropdown Item 3</sl-menu-item>
+                    <sl-divider></sl-divider>
+                    <sl-menu-item checked>Checked</sl-menu-item>
+                    <sl-menu-item disabled>Disabled</sl-menu-item>
+                    <sl-divider></sl-divider>
+                    <sl-menu-item>
+                        Prefix
+                        <sl-icon slot="prefix" name="gift"></sl-icon>
+                    </sl-menu-item>
+                    <sl-menu-item>
+                        Suffix Icon
+                        <sl-icon slot="suffix" name="heart"></sl-icon>
+                    </sl-menu-item>
+                </sl-menu>
+            </sl-dropdown>
             <sl-button size="large">Click me</sl-button>
+            <sl-color-picker value="#4a90e2" label="Select a color"/>
+            </div>
         <table>
             {tableRows}
         </table>
